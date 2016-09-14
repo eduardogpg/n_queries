a3e433cded784eed0f9cc8bfa74552a10fcb8601
Eduardo Ismael <eduardo78d@gmail.com>
2016-09-14 14:40:25 -0500

    show custome countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,7 +4,7 @@ class CountriesController < ApplicationController
   # GET /countries
   # GET /countries.json
   def index
-    #@countries = Country.all
+    @countries = Country.all
     @countries_join = Country.get_state
   end
 
--- app/models/country.rb
+++ app/models/country.rb
@@ -2,6 +2,6 @@ class Country < ActiveRecord::Base
 	has_many :states
 
 	def self.get_state
-		self.joins(:states).select("countries.name, states.name as state_name")
+		self.joins(:states).select("countries.name, countries.id as countrie_id, states.name as state_name")
 	end
 end
